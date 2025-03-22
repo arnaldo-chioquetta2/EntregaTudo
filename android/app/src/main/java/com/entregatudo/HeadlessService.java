77c21162336a95bfce3555e669125095a9ff9719
Xeviousbr <xeviousbr@gmail.com>
2025-03-22 00:28:04 -0300

    Voltou a funcionar o App

---
+++ android/app/src/main/java/com/entregatudo/HeadlessService.java
@@ -0,0 +1,14 @@
+package com.example.entregatudo; // Substitua pelo seu package real
+
+import android.content.Context;
+import android.util.Log;
+
+// import com.background_fetch.BackgroundFetchHeadlessTask;
+
+// public class HeadlessService implements BackgroundFetchHeadlessTask {
+    // @Override
+    // public void onHeadlessTask(Context context, String taskId) {
+        // Log.d("HeadlessService", "Tarefa em background: " + taskId);
+        // // Adicione sua lógica aqui
+    // }
+// }
